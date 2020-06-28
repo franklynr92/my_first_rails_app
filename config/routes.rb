5fca9dccf1893a29ac5da271058e481f90a8dee4
Franklyn Rodriguez <franklynr92@gmail.com>
2020-06-28 15:33:13 -0400

    fixed migration issues by running command

--- config/routes.rb
+++ config/routes.rb
@@ -2,33 +2,3 @@ Rails.application.routes.draw do
   root 'cars#index'
   resources :cars
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-#resources :cars #/cars same thing on browser this is what will be displayed
- # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- =end
